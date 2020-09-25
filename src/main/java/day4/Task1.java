c81bfe59aa22fda24bacb41ddd4a73fe1d2779e6
Sayat Rakhimov <flat701@inbox.ru>
2020-09-25 16:06:14 +0600

    complete day4 Task1

--- src/main/java/day4/Task1.java
+++ src/main/java/day4/Task1.java
@@ -1,7 +1,54 @@
 package day4;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
 
+public class Task1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int arrayLength = Integer.parseInt(reader.readLine());
+        int[] array = new int[arrayLength];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(11);
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+        System.out.println("длина массива: " + array.length);
+
+        int moreThanEight = 0;
+        int one = 0;
+        int even = 0;
+        int odd = 0;
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+            if (array[i] > 8)
+                moreThanEight++;
+            else if (array[i] == 1)
+                one++;
+            else if (array[i] % 2 == 0) // с четными/нечетными вроде правильно пишу код, но вывод не правильный
+                even++;
+            else if (array[i] % 2 != 0)
+                odd++;
+        }
+
+        System.out.println("Количество чисел больше 8: " + moreThanEight);
+        System.out.println("Количество чисел равных 1: " + one);
+        System.out.println("Количество четных чисел: " + even);
+        System.out.println("Количество нечетных чисел: " + odd);
+        System.out.println("Сумма всех элементов массива: " + sum);
     }
 }
+
+
+
+
+
+
+
+
+
--- src/main/java/day4/Task2.java
+++ src/main/java/day4/Task2.java
@@ -1,7 +1,37 @@
 package day4;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
 
+        int[] numbers = new int[15];
+        Random random = new Random();
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int count = 0;
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(1001);
+        }
+
+        for (int n : numbers) {
+            if (n > max)
+                max = n;
+            else if (n < min)
+                min = n;
+            else if (n % 10 == 0) { // когда в массиве встречается одна цифра заканчивающаяся
+                count++;            // на 0, то счетчик count и sum выводит корректно, но если
+                sum = sum + n;      // более одной то результат выходит ошибочный. Подскажите пжл в чем дело
+            }
+
+        }
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(max);
+        System.out.println(min);
+        System.out.println(count);
+        System.out.println(sum);
     }
 }
