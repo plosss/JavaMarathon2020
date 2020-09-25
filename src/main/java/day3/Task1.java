f341c6144288034661f05590c6e5555acfb89cfd
Sayat Rakhimov <flat701@inbox.ru>
2020-09-25 16:19:29 +0600

    update day3 Task1

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -7,33 +7,39 @@ import java.io.InputStreamReader;
 public class Task1 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String cities = reader.readLine();
 
-        switch (cities) {
-            case "стоп":
+
+        while (true) {
+            String cities = reader.readLine();
+
+            if (cities.equals("Стоп"))
                 break;
-            case "Москва":
-            case "Владивосток":
-            case "Ростов":
-                System.out.println("Россия");
-                break;
-            case "Рим":
-            case "Милан":
-            case "Турин":
-                System.out.println("Италия");
-                break;
-            case "Ливерпуль":
-            case "Манчестер":
-            case "Лондон":
-                System.out.println("Англия");
-                break;
-            case "Берлин":
-            case "Мюнхен":
-            case "Кёльн":
-                System.out.println("Германия");
-                break;
-            default:
-                System.out.println("Неизвестная страна");
+
+            switch (cities) {
+                case "Москва":
+                case "Владивосток":
+                case "Ростов":
+                    System.out.println("Россия");
+                    break;
+
+                case "Рим":
+                case "Милан":
+                case "Турин":
+                    System.out.println("Италия");
+                    break;
+                case "Ливерпуль":
+                case "Манчестер":
+                case "Лондон":
+                    System.out.println("Англия");
+                    break;
+                case "Берлин":
+                case "Мюнхен":
+                case "Кёльн":
+                    System.out.println("Германия");
+                    break;
+                default:
+                    System.out.println("Неизвестная страна");
+            }
         }
     }
 }
