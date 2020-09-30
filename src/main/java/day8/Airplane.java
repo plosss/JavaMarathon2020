afb1abd374c1ca17eb73b16a41553488f4d960de
Sayat Rakhimov <flat701@inbox.ru>
2020-10-01 00:01:00 +0600

    Complete day8 tasks

---
+++ src/main/java/day8/Airplane.java
@@ -0,0 +1,74 @@
+package day8;
+
+public class Airplane {
+    private String producer;
+    private int year;
+    private int length;
+    private int weight;
+    private int fuel;
+
+    public Airplane(String producer, int year, int length, int weight) {
+        this.producer = producer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+    }
+
+    public void info() {
+        System.out.println("Изготовитель: " + producer + ", год выпуска: " + year + ", длина: "
+                + length + ", вес: " + weight);
+    }
+
+    public void fillUp(int inputFuel) {
+        fuel += inputFuel;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+
+    public String toString() {
+        return "Изготовитель: " + producer + ", год выпуска: " + year + ", длина: "
+                + length + ", вес: " + weight;
+    }
+}
+
+
+
+
--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,30 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String s = "";
 
+        long st = System.currentTimeMillis();
+
+        for (int i = 0; i < 20001; i++) {
+            s = i + " ";
+        }
+
+        long ft = System.currentTimeMillis();
+
+        System.out.println("время обработки через String: " + (ft - st));
+
+        StringBuilder s1 = new StringBuilder();
+
+        long st2 = System.currentTimeMillis();
+
+        for (int i = 0; i < 20001; i++) {
+            s1.append(i).append(" ");
+        }
+
+        long ft2 = System.currentTimeMillis();
+
+        System.out.println("время обработки через StringBuilder: " + (ft2 - st2));
     }
 }
+
+
