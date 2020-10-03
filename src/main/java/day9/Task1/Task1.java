07085540a7c5132fb89ca46f80c9c555ccdd2f75
Sayat Rakhimov <flat701@inbox.ru>
2020-10-03 11:56:13 +0600

    Completed 9day tasks

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -1,7 +1,17 @@
 package day9.Task1;
 
+import day9.Student;
+import day9.Teacher;
+
 public class Task1 {
     public static void main(String[] args) {
+        Student student = new Student("Bil","PO");
+        Teacher teacher = new Teacher("John", "history");
 
+        student.printInfo();
+        teacher.printInfo();
+
+        System.out.println(student.getGroup());
+        System.out.println(teacher.getSubject());
     }
 }
